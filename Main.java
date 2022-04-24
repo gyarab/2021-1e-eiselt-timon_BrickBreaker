5172ed5340eec654ed50eedefb258626a7867854
TimEiselt <94890295+TimEiselt@users.noreply.github.com>
2022-04-24 15:22:31 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,12 +4,12 @@ public class Main {
 
     public static void main (String [] args) {
 
-        JFrame objekt = new JFrame ();
+        JFrame objekt = new JFrame (); //vytváření objektu//
 
         Hratelnost hratelnost = new Hratelnost ();
 
-        objekt.setBounds (10, 10, 710, 600);
-        objekt.setTitle ("Vítejte ve hře BRICK BREAKER");
+        objekt.setBounds (10, 10, 710, 600); //udávání rozměrů otevřeného okna//
+        objekt.setTitle ("Vítejte ve hře BRICK BREAKER"); //nadpis v otevřeném okně//
         objekt.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
         objekt.add (hratelnost);
         objekt.setResizable (false);
